f0303ce2c18440f4a24f0b0892daa3f6cdf7ead2
Till Grallert <till.grallert@fu-berlin.de>
2016-05-07 09:21:36 +0300

    initial commit of the map showing publication places for reviewed works

---
+++ assets/dape/maps/cities.js
@@ -0,0 +1,132 @@
+{
+    "items":[
+            {
+            "label":"Aleppo",
+            "type":"city",
+            "latlng":"36.20124, 37.16117",
+            "periodicals":"1"},
+            {
+            "label":"Baghdad",
+            "type":"city",
+            "latlng":"33.34058, 44.40088",
+            "periodicals":"9"},
+             {
+            "label":"Beirut",
+            "type":"city",
+            "latlng":"33.88894, 35.49442",
+            "periodicals":"12"},
+                {
+            "label":"Brazil",
+            "type":"country",
+            "latlng":"-10, -55",
+            "periodicals":"1"},
+             {
+            "label":"Cairo",
+            "type":"city",
+            "latlng":"30.06263, 31.24967",
+            "periodicals":"32"},
+                {
+            "label":"Damascus",
+            "type":"city",
+            "latlng":"33.5102, 36.29128",
+            "periodicals":"5"},
+                 {
+            "label":"Istanbul",
+            "type":"city",
+            "latlng":"41.01384, 28.94966",
+            "periodicals":"4"},
+                {
+            "label":"Jounieh / Jūnī (Lebanon)",
+            "type":"town",
+            "latlng":"33.9877, 35.638924",
+            "periodicals":"1"},
+                {
+            "label":"Lebanon",
+            "type":"country",
+            "latlng":"33.83333, 35.83333",
+            "periodicals":"1"},
+             {
+            "label":"London",
+            "type":"city",
+            "latlng":"51.50853, -0.12574",
+            "periodicals":"1"},
+                {
+            "label":"New York",
+            "type":"city",
+            "latlng":"40.71427, -74.00597",
+            "periodicals":"2"},
+                 {
+            "label":"Paris",
+            "type":"city",
+            "latlng":"48.85341, 2.3488",
+            "periodicals":"6"},
+               {
+            "label":"Sao Paulo",
+            "type":"city",
+            "latlng":"-23.5475, -46.63611",
+            "periodicals":"1"},
+               {
+            "label":"Tripoli",
+            "type":"city",
+            "latlng":"32.87519, 13.18746",
+            "periodicals":"2"},
+                {
+            "label":"Tunis",
+            "type":"city",
+            "latlng":"36.81897, 10.16579",
+            "periodicals":"1"},
+               
+               {
+                  "label":"Batroun",
+                  "type":"city",
+                  "latlng":"34.25528, 35.65806",
+                  "periodicals":"1"
+               },
+               {
+                  "label":"Homs",
+                  "type":"city",
+                  "latlng":"34.72682, 36.72339",
+                  "periodicals":"1"
+               },
+               {
+                  "label":"Haifa",
+                  "type":"city",
+                  "latlng":"32.81841, 34.9885",
+                  "periodicals":"2"
+               },
+               {
+                  "label":"Leiden",
+                  "type":"city",
+                  "latlng":"52.15833, 4.49306",
+                  "periodicals":"2"
+               },
+               {
+                  "label":"Singapore",
+                  "type":"city",
+                  "latlng":"1.28967, 103.85007",
+                  "periodicals":"1"
+               },
+               {
+                  "label":"Zahle",
+                  "type":"city",
+                  "latlng":"33.84972, 35.90417",
+                  "periodicals":"1"
+               },
+               {"label":"Jaffa", 
+               "type": "city", 
+               "latlng": "32.05043, 34.75224",
+               "periodicals": "1"}],
+	"types": {
+		"City": {
+			"pluralLabel": "Cities"
+		}
+	},
+	"properties": {
+		"rank": {
+			"valueType": "number"
+		},
+		"periodicals": {
+			"valueType": "number"
+		}
+	}
+}
